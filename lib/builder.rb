bb7ef415fccafee1667ca3400199e9c5b8509e9c
AKnopf <marvinede@gmx.net>
2012-12-12 12:46:50 +0100

    enhanced documentation

--- lib/builder.rb
+++ lib/builder.rb
@@ -1,6 +1,16 @@
 module Traits
   # The builder class implements the builder pattern for incorporations. It defines a nice and clean syntax for
   # incorporating traits into classes or traits
+  # @example
+  #   class Monster
+  #     incorporate.
+  #       traits(:movable,hittable,:emotion).
+  #     resolve(:update,:draw).
+  #       with_pattern.call_in_order.
+  #     resolve(:moved?).
+  #       manually{raise "Not clear what to do, think and re-implement"}.
+  #     done
+  #   end
   class Builder
 
     private
@@ -24,6 +34,21 @@ module Traits
 
     public
 
+    # Defines that a trait shall be incorporated.
+    # @param [#to_trait] trait An object that can be converted to a #Trait.
+    #   In the default implementation these are:
+    #   * #Symbol
+    #   * #Module
+    #   * #Trait
+    # @precondition {#trait} was called
+    # @precondition {#traits} was called
+    # @precondition {#with_options} was called
+    # @precondition nothing was called
+    # @postcondition {#trait} may be called
+    # @postcondition {#traits} may be called
+    # @postcondition {#with_options} may be called
+    # @postcondition {#resolve} may be called
+    # @postcondition {#done} may be called
     def trait(trait)
       if call_stack.last == :trait
         call_stack.pop 2
@@ -39,6 +64,25 @@ module Traits
       end
     end
 
+    # Defines options under which a trait is incorporated into a class
+    # @param [Hash<Symbol,Array<Symbol>>] hash A hash with the options.
+    #
+    #   Valid keys are +:except+ and +:only+.
+    #
+    #   The value is an Array of method names that will be treated like this:
+    # [+:except+] excluded from the incorporation
+    # [+:only+] exclusively included into the incorporation
+    #
+    # @precondition {#trait} was called
+    # @precondition {#traits} was called
+    # @postcondition {#trait} may be called
+    # @postcondition {#traits} may be called
+    # @postcondition {#resolve} may be called
+    # @postcondition {#done} may be called
+    # @example
+    #   builder.trait(:moving).with_options({except: [:moved?,:draw]}).done
+    #
+    #   builder.trait(:moving).with_options({only: [:move_relative,:move_absolute]}).done
     def with_options(hash)
       if call_stack.last == :trait
         call_stack.pop
@@ -49,46 +93,107 @@ module Traits
       end
     end
 
+    # Defines that some methods of the latest defined trait are not incorporated.
+    # @param [Array<Symbol>] methods Variable amount of method names to be excluded from
+    #   the incorporation
+    # @see #with_options
+    # @note This method is just a shortcut for +with_options(:except => methods)+. It leads to
+    #   more readable code
     def except(*methods)
       with_options(except: methods)
     end
 
+    # Defines that some methods of the latest defined trait are exclusively incorporated.
+    # @param [Array<Symbol>] methods Variable amount of method names to be exclusively included
+    #   into the incorporation
+    # @see #with_options
+    # @note This method is just a shortcut for +with_options(:only => methods)+. It leads to
+    #   more readable code
     def only(*methods)
       with_options(only: methods)
     end
 
     alias_method :but_only, :only
 
+    # Defines that multiple traits me be called.
+    # @param traits [Array<#to_trait>] An array with objects that can be converted into a trait.
+    # @note For more information about #to_trait objects as well as pre and post conditions,
+    #   see: {#trait}
     def traits(*traits)
       traits.each { |trait| trait(trait) }
       self
     end
 
-    def resolve(symbol)
-      if call_stack.last == :list_traits
-        call_stack.pop
-        call_stack << :list_resolves
-        resolve(symbol)
-      elsif call_stack.last == :trait
-        call_stack.pop 2
-        resolve(symbol)
-      elsif call_stack.last == :list_resolves
-        call_stack << symbol
-        call_stack << :resolve
-        self
+    # @overload resolve(symbol)
+    #   Defines the behavior of the resolved implementation of the method
+    #   with the name in +symbol+
+    #   @param [Symbol] symbol The name of the method to be resolved
+    #   @precondition At least one trait is defined. {#trait} or {#traits} was called
+    #   @postcondition A concrete resolve must be defined after this method. {#with_lambda}
+    #     or {#with_pattern} must be called.
+    #   @example
+    #     builder.resolve(:update).with_pattern.<more methods>
+    def resolve(*symbol)
+      #--
+      # The following overload of this method is half hearty implemented but not really ready for use
+      # Thus it is not visible to in the documentation
+      # @overload resolve(*symbols)
+      #   Defines the behavior if the resolved implementation of multiple methods
+      #   with the names in +symbols+
+      #   @param [Array<Symbol>] symbols Varargs with the names of the methods to be resolved
+      #   @precondition At least one trait is defined. {#trait} or {#traits} was called
+      #   @postcondition A concrete resolve must be defined after this method. {#with_lambda}
+      #     or {#with_pattern} must be called.
+      #   @example
+      #     builder.resolve(:update).with_pattern.<more methods>
+      #--
+      if symbol.length > 1
+        symbol.each { |symbol| resolve(symbol) }
       else
-        raise 'You may not call "resolve" here. Maybe you called "resolve" twice in a row? ' +
-                  'You need to call "with_lambda" or "with_pattern" first.'
+        symbol = symbol[0]
+        if call_stack.last == :list_traits
+          call_stack.pop
+          call_stack << :list_resolves
+          resolve(symbol)
+        elsif call_stack.last == :trait
+          call_stack.pop 2
+          resolve(symbol)
+        elsif call_stack.last == :list_resolves
+          call_stack << symbol
+          call_stack << :resolve
+          self
+        else
+          raise 'You may not call "resolve" here. Maybe you called "resolve" twice in a row? ' +
+                    'You need to call "with_lambda" or "with_pattern" first.'
+        end
       end
     end
 
     alias_method :resolves, :resolve
 
 
-    def with_lambda(proc = nil,&block)
+    # Defines a concrete implementation that is used to resolve a method
+    # @param [Proc] proc The implementation can be defined as a Proc object as only parameter
+    # @yield [*args] The implementation can also be defined as a block to this method. If the
+    #   parameter +proc+ is set, it takes precedence.
+    # @precondition {#resolve} was called
+    # @postcondition {#resolve} or {#done} can be called
+    # @example
+    #   builder.<some methods>.
+    #     resolve(:my_method).
+    #       with_lambda {my_method_in_trait_one + my_method_in_trait_two}. #using a block
+    #     <further methods>.
+    #   done
+    # @example
+    #   builder.<some methods>.
+    #     resolve(:my_method).
+    #       with_lambda(-> {my_method_in_trait_one + my_method_in_trait_two}). #using a Proc object
+    #     <further methods>.
+    #   done
+    def with_lambda(proc = nil, &block)
       if call_stack.last == :resolve
         call_stack.pop
-        proc ||= block
+        proc                          ||= block
         resolves_accu[call_stack.pop] = proc
         self
       else
@@ -98,6 +203,10 @@ module Traits
 
     alias_method :manually, :with_lambda
 
+    # Defines a concrete implementation that is used to resolve a method in form of a pattern
+    # @precondition {#resolve} was called
+    # @postcondition One of the pattern methods is called. In this moment these are:
+    #   * {#call_in_order}
     def with_pattern
       if call_stack.last == :resolve
         call_stack << :pattern
@@ -107,6 +216,12 @@ module Traits
       end
     end
 
+    # Defines a concrete implementation to resolve a method in the form that all versions of
+    # the method are called in an order
+    # @param [Array<Symbol>] order The order in which the different versions are called.
+    #   The default value is the order in which the traits were defined
+    # @precondition {#with_pattern} was called
+    # @postcondition {#resolve} or {#done} are called
     def call_in_order(order = traits_accu.keys)
       if call_stack.last == :pattern
         order = order.collect { |trait| Trait[trait] }
@@ -124,6 +239,10 @@ module Traits
       end
     end
 
+    # Triggers the actual incorporation of traits
+    # @precondition Traits defined
+    # @precondition All conflicts resolved
+    # @raise [RuntimeError] When there are unsolved conflicts
     def do_it
       build.incorporate
     end
@@ -132,10 +251,14 @@ module Traits
     alias_method :incorporate, :do_it
 
 
+    # Syntactic sugar
     def and
       self
     end
 
+    # Returns the incorporation object without applying it to the incorporator.
+    # @note Do not use this unless you know what you do. If you just want to use traits, use the
+    #   {#done} method instead.
     def build
       Incorporation.new(traits_accu, resolves_accu, incorporator)
     end
